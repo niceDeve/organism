b2c1be74d7c14ca4bb160c7f8f9951ab1c0d01cd
nicedeve <bdawn0878@gmail.com>
2020-10-12 15:46:00 +0200

    BLOCKOTUS/organism#66 add webapp.test

--- __tests__/webapp.test.js
+++ __tests__/webapp.test.js
@@ -1,3 +1,12 @@
-it('Should not complain because there is no test yet', () => {
+/**
+ * @jest-environment node
+ */
 
+const axios = require('axios');
+
+const WEBAPP_URL = 'http://127.0.0.1:5000/';
+
+it('Should connect to the webapp, status 200', async () => {
+    const request = await axios.get(WEBAPP_URL);
+    expect(request.status).toBe(200);
 });
