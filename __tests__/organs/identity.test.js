8ea6661db46ec4a8f12a797b140e90a04e10fbd6
nicedeve <bdawn0878@gmail.com>
2020-11-01 12:25:27 +0100

    BLOCKOTUS/organism#46 update organs.identity.test (#79)
    * BLOCKOTUS/organism#46 update organs.identity.test
    
    * update version

--- __tests__/organs/identity.test.js
+++ __tests__/organs/identity.test.js
@@ -1,11 +1,48 @@
 const fs = require('fs');
 const path = require('path');
+const { RSA, Crypt } = require('hybrid-crypto-js');
+const crypto = require('crypto');
 
 const registerUser = require('../../organs/admins/registerUser').main;
 const api = require('../../organs/identity/api');
-var user;
+
+var rsa = new RSA();
+var crypt = new Crypt();
+
+const identity1 = {
+    firstname: 'John',
+    lastname: 'Smith',
+    birthdate: '1992-01-01',
+    nation: 'ES',
+    nationalId: '89898989G',
+};
+
+const identity2 = { ...identity1, nationalId: '78787878K' };
+
+var user, keypair1, keypair2, encryptedIdentity1, encryptedIdentity2, uniqueHash1, uniqueHash2;
+
+const generateKeyPair = () => {
+    return new Promise((resolve) => {
+        rsa.generateKeyPair(resolve);
+    });
+};
+
+const uniqueHashFromIdentity = identity =>
+    crypto
+        .createHash('md5')
+        .update(`${identity.nation}-${identity.nationalId}-${identity.birthdate}`)
+        .digest('hex');
 
 beforeAll( async () => {
+    // get a keypair and encrypt the identity with the publicKey
+    keypair1 = await generateKeyPair();
+    keypair2 = await generateKeyPair();
+    encryptedIdentity1 = crypt.encrypt(keypair1.publicKey, JSON.stringify(identity1));
+    encryptedIdentity2 = crypt.encrypt(keypair2.publicKey, JSON.stringify(identity2));
+    uniqueHash1 = uniqueHashFromIdentity(identity1);
+    uniqueHash2 = uniqueHashFromIdentity(identity2);
+
+    // register a user in the network
     const username = (Math.random() * 420).toString();
     await registerUser(username);
     const wallet = fs.readFileSync(path.join(__dirname, `../../wallet/${username}.id`), {encoding: 'utf8'});
@@ -14,13 +51,15 @@ beforeAll( async () => {
 
 it('Should create an identity', async () => {
     var isError = false;
-    const identity = await api.create({
-        encryptedIdentity: {},
+    await api.create({
+        encryptedIdentity: encryptedIdentity1,
+        uniqueHash: uniqueHash1,
         override: true,
         user
     }).catch(e => isError = true);
-    const identity2 = await api.create({
-        encryptedIdentity: {},
+    await api.create({
+        encryptedIdentity: encryptedIdentity2,
+        uniqueHash: uniqueHash2,
         override: true,
         user
     }).catch(e => isError = true);
@@ -30,8 +69,9 @@ it('Should create an identity', async () => {
 
 it('Should not be able to create an identity', async () => {
     var isError = false;
-    const identity = await api.create({
-        encryptedIdentity: {},
+    await api.create({
+        encryptedIdentity: encryptedIdentity2,
+        uniqueHash: uniqueHash2,
         override: false,
         user
     }).catch(e => isError = true);
